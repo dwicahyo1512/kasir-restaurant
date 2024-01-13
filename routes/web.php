8bf73ef0bc2b86f514c9ebe299015c7db20c1323
dwi cahyo <dwicahyo.1512@gmail.com>
2024-01-13 16:05:25 +0700

    add component menu and asset metronic

--- routes/web.php
+++ routes/web.php
@@ -40,9 +40,10 @@ Route::middleware('splade')->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+        Route::resource('users', App\Http\Controllers\UserController::class);
     });
 
-    Route::resource('users', App\Http\Controllers\UserController::class);
 
     require __DIR__.'/auth.php';
 });
